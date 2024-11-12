54b5f158a5b57408b7c5869876c63bc167d8d568
Adriana Osorio <driosorio@gmail.com>
2024-11-12 19:54:55 -0300

    added ROO gem and State model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+require 'roo'
+
+file_name = './lib/seeds/municipalities.xlsx'
+xlsx = Roo::Spreadsheet.open(file_name)
+xlsx.info
+# => Returns basic info about the spreadsheet file
