271360e0aadc3835950efca4ee43e25e3498ab99
Adriana Osorio <driosorio@gmail.com>
2024-11-17 16:18:52 -0300

    banco criado com associacoes

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -4,6 +4,6 @@ class Municipality < ApplicationRecord
   belongs_to :user, optional: true
   has_many :phones, as: :callable
   has_many :emails, as: :emailable
-
+  has_many :enrollments
   has_many :providers, through: :enrollments
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,6 @@
 class Provider < ApplicationRecord
   has_many :phones, as: :callable
   has_many :emails, as: :emailable
-
+  has_many :enrollments
   has_many :municipalities, through: :enrollments
 end
