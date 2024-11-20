c0ffe63cc76ded727b072f36e55b67023c6bc2d0
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-20 17:24:51 -0300

    Add new/create action to providers controller

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,10 +1,23 @@
 class ProvidersController < ApplicationController
   def new
+    @provider = Provider.new
   end
 
   def create
+    @provider = Provider.new(provider_params)
+    if @provider.save 
+      redirect_to root_path 
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+
+  def provider_params
+    params.require(:provider).permit(:name, :cnpj, :site_url, :contact_name, :acceptance_date)
   end
 end
