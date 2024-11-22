7aeebaa27f575a8eba1b40079c8f0c48736fd341
Adriana Osorio <driosorio@gmail.com>
2024-11-21 23:43:41 -0300

    refazendo o seed de municipios

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,39 +32,25 @@ def create_batch(date_start, date_end)
 end
 
 def create_municipality(file_name, default_sheet)
-  xlsx = Roo::Spreadsheet.open(file_name)
+  # xlsx = Roo::Spreadsheet.open(file_name)
+  # xlsx.default_sheet = xlsx.sheets[default_sheet]
+  # puts " Default sheet: #{xlsx.default_sheet}"
+  xlsx = Roo::Excelx.new(file_name)
   xlsx.default_sheet = xlsx.sheets[default_sheet]
   puts " Default sheet: #{xlsx.default_sheet}"
-  # uf	name	coord-ori	corrd-current	contact-name	contact-title	contact-phone
-  # contact-email	numer-of-attempts	date-last-attempt	contact-effective	date-memo-sent
-  # "name"
-  # "contact_name"
-  # "contact_title"
-  # "original_coordinator"
-  # "number_of_attempts"
-  # "date_last_attempt"
-  # "contact_effective"
-  # "official_letter_sent"
-  # "capital_city"
-  # "state_id"
-  # "batch_id"
-  # "user_id"
+  # 0-uf	1-name	2-coord-ori	3-corrd-current	4-contact-name	5-contact-title	6-contact-phone
+  # 7-contact-email	8-number-of-attempts	9-date-last-attempt	10-contact-effective	11-date-memo-sent
+  # "name", "contact_name", "contact_title", "original_coordinator", "number_of_attempts"
+  # "date_last_attempt", "contact_effective", "official_letter_sent", "capital_city", "state_id"
+  # "batch_id", "user_id"
   #
-   xlsx.each(uf: 'uf',
-            name: "name",
-            contact_name: "contact-name",
-            contact_title: "contact-title",
-            original_coordinator: "coord-ori",
-            number_of_attempts: "corrd-current",
-            date_last_attempt: "date-last-attempt",
-            contact_effective: "contact-effective",
-            official_letter_sent: "date-memo-sent",
-            ) do | row |
-     p row
-     p row[:name]
-   end
-  xlsx.each_row_streaming do |row|
-    puts row[3]
+  xlsx.each_row_streaming(offset: 1) do |row|
+    # puts row
+    user_current = row[3]
+    user_ori = row[2]
+    state_code = row[0]
+    state = State.where(code: state_code)
+    puts "state do find_by: #{state.id_value}"
   end
 end
 
@@ -72,80 +58,69 @@ puts "*****************"
 puts "Cleaning the database"
 puts "*****************"
 
-State.destroy_all
-Batch.destroy_all
-Enrollment.destroy_all
-Phone.destroy_all
-Email.destroy_all
-Provider.destroy_all
-Municipality.destroy_all
-User.destroy_all
+# State.destroy_all
+# Batch.destroy_all
+# Enrollment.destroy_all
+# Phone.destroy_all
+# Email.destroy_all
+# Provider.destroy_all
+# Municipality.destroy_all
+# User.destroy_all
 
 puts "************************"
 puts 'Creating users'
 puts "************************"
 
-create_user('Carlos', 'Siqueira', 'carlos.siqueira@sevabrasil.com', 'Admin')
-
-ailtom = create_user('Ailtom', 'Gobira', 'gobira@sevabrasil.com', 'Coordinator')
-
-create_user('João', 'Viríssimo', 'joao@sevabrasil.com', 'Coordinator')
-
-create_user('Mara', 'Francy', 'mara@sevabrasil.com', 'Coordinator')
-
-create_user('Marcello', 'Santo', 'marcelo@sevabrasil.com', 'Coordinator')
-
-create_user('Márcia', 'Cavalcante', 'marcia@sevabrasil.com', 'Coordinator')
-
-@users_hash.each do |key, value|
-  puts "User #{key} is #{value}"
-end
+# create_user('Carlos', 'Siqueira', 'carlos.siqueira@sevabrasil.com', 'Admin')
+# create_user('Ailtom', 'Gobira', 'gobira@sevabrasil.com', 'Coordinator')
+# create_user('João', 'Viríssimo', 'joao@sevabrasil.com', 'Coordinator')
+# create_user('Mara', 'Francy', 'mara@sevabrasil.com', 'Coordinator')
+# create_user('Marcello', 'Santo', 'marcelo@sevabrasil.com', 'Coordinator')
+# create_user('Márcia', 'Cavalcante', 'marcia@sevabrasil.com', 'Coordinator')
 
 puts "************************"
 puts 'Creating states'
 puts "************************"
 
-create_state('RJ', 'Rio de Janeiro')
-create_state('AC', 'Acre')
-create_state('AL', 'Alagoas')
-create_state('AP', 'Amapá')
-create_state('AM', 'Amazonas')
-create_state('BA', 'Bahia')
-create_state('CE', 'Ceará')
-create_state('ES', 'Espírito Santo')
-create_state('GO', 'Goiás')
-create_state('MA', 'Maranhão')
-create_state('MT', 'Mato Grosso')
-create_state('MS', 'Mato Grosso do Sul')
-create_state('MG', 'Minas Gerais')
-create_state('PA', 'Pará')
-create_state('PB', 'Paraíba')
-create_state('PR', 'Paraná')
-create_state('PE', 'Pernambuco')
-create_state('PI', 'Piauí')
-create_state('RJ', 'Rio de Janeiro')
-create_state('RN', 'Rio Grande do Norte')
-create_state('RS', 'Rio Grande do Sul')
-create_state('RO', 'Rondônia')
-create_state('RR', 'Roraima')
-create_state('SC', 'Santa Catarina')
-create_state('SP', 'São Paulo')
-create_state('SE', 'Sergipe')
-create_state('TO', 'Tocantins')
+# create_state('RJ', 'Rio de Janeiro')
+# create_state('AC', 'Acre')
+# create_state('AL', 'Alagoas')
+# create_state('AP', 'Amapá')
+# create_state('AM', 'Amazonas')
+# create_state('BA', 'Bahia')
+# create_state('CE', 'Ceará')
+# create_state('ES', 'Espírito Santo')
+# create_state('GO', 'Goiás')
+# create_state('MA', 'Maranhão')
+# create_state('MT', 'Mato Grosso')
+# create_state('MS', 'Mato Grosso do Sul')
+# create_state('MG', 'Minas Gerais')
+# create_state('PA', 'Pará')
+# create_state('PB', 'Paraíba')
+# create_state('PR', 'Paraná')
+# create_state('PE', 'Pernambuco')
+# create_state('PI', 'Piauí')
+# create_state('RJ', 'Rio de Janeiro')
+# create_state('RN', 'Rio Grande do Norte')
+# create_state('RS', 'Rio Grande do Sul')
+# create_state('RO', 'Rondônia')
+# create_state('RR', 'Roraima')
+# create_state('SC', 'Santa Catarina')
+# create_state('SP', 'São Paulo')
+# create_state('SE', 'Sergipe')
+# create_state('TO', 'Tocantins')
 
 puts "************************"
 puts 'Creating batches'
 puts "************************"
 
-create_batch('20240916', '20241017')
-create_batch('20240930', '20241017')
+# create_batch('20240916', '20241017')
+# create_batch('20240930', '20241017')
 
-# puts "************************"
-# puts 'Creating municipalities'
-# puts "************************"
+puts "************************"
+puts 'Creating municipalities'
+puts "************************"
 
-# create_municipality('./lib/seeds/municipalities/Lista2.xlsx', 0)
-
-
+create_municipality('./lib/seeds/municipalities/Lista2.xlsx', 0)
 
 puts "Seeding completed (❁´◡`❁)"
