46341832f7a71a25869842e52084fa226221ab1a
Adriana Osorio <driosorio@gmail.com>
2024-11-25 22:04:23 -0300

    ainda com erro no create municipality - date format

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,13 +25,14 @@ def create_state(code, name)
 end
 
 def create_batch(date_start, date_end)
-  batch = Batch.new(start_date: date_start, end_date: date_end)
-  batch.save!
+  batch = Batch.create! :start_date => date_start,
+                        :end_date => date_end
 end
 
 def create_municipality(file_name, default_sheet, batch_id)
   xlsx = Roo::Excelx.new(file_name)
   xlsx.default_sheet = xlsx.sheets[default_sheet]
+  puts "Batchid = #{batch_id}"
   # puts " Default sheet: #{xlsx.default_sheet}"
   # 0-uf	1-name	2-coord-ori	3-corrd-current	4-contact-name	5-contact-title	6-contact-phone
   # 7-contact-email	8-number-of-attempts	9-date-last-attempt	10-contact-effective	11-date-memo-sent
@@ -39,13 +40,14 @@ def create_municipality(file_name, default_sheet, batch_id)
   # "date_last_attempt", "contact_effective", "official_letter_sent", "capital_city", "state_id"
   # "batch_id", "user_id"
   rows = 0
-  xlsx.each_row_streaming(offset: 1) do |row|
-    puts row[9].type
+  xlsx.each_row_streaming(offset: 1, pad_cells: true, max_rows: 2) do |row|
+    puts "row: #{rows}"
     # busca o state_id
     state_code = row[0].to_s
     state = State.find_by code: state_code
     unless state_code.nil?
       state_id = state.id
+      puts "state code: #{state_code} e state id = #{state_id} e name = #{row[1]}"
     end
 
     # busca user_id do coordenador original e atual
@@ -61,17 +63,29 @@ def create_municipality(file_name, default_sheet, batch_id)
     end
 
     # prepara contact effective?
-    # condition ? code_when_truthy : code_when_falsey
-    row[10].to_s.upcase == "S" ? contact_effective = true :  contact_effective = false
+    contact_effective = false
+    if row[10].to_s == "s" || row[10].to_s == "S"
+        contact_effective = true
+    end
 
+    #prepara number of atttempts
+
+
+    #prepara datas
+    puts "data last attempt lida: #{row[9]} com tipo #{row[9].type}"
+    puts "data memo sent lida: #{row[9]} com tipo #{row[9].type}"
+    puts "batch id: #{batch_id}"
+    puts "number of atttempts lido: #{row[8]} com tipo #{row[8].type}"
+    date_last_attempt = row[9]
+    puts date_last_attempt.type
     municipality = Municipality.create! :name => row[1],
                                         :contact_name => row[4],
                                         :contact_title => row[5],
                                         :original_coordinator => user_id_original,
                                         :number_of_attempts => row[8],
-                                        :date_last_attempt => row[9].to_date,
+                                        :date_last_attempt => "10-14-24",
                                         :contact_effective => row[10],
-                                        :official_letter_sent => row[11].to_date,
+                                        :official_letter_sent => "10-14-24",
                                         :capital_city => false,
                                         :state_id => state_id,
                                         :batch_id => batch_id,
@@ -87,12 +101,12 @@ puts "Cleaning the database"
 puts "*****************"
 
 # State.destroy_all
-Batch.destroy_all
+# Batch.destroy_all
 # Enrollment.destroy_all
 # Phone.destroy_all
 # Email.destroy_all
 # Provider.destroy_all
-# Municipality.destroy_all
+Municipality.destroy_all
 # User.destroy_all
 
 puts "************************"
@@ -144,6 +158,7 @@ puts "************************"
 
 lista_1 = create_batch('20240916', '20241017')
 lista_2 = create_batch('20240930', '20241017')
+puts lista_2
 
 puts "************************"
 puts 'Creating municipalities'
