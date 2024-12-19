0e4cd34652d671aa962ef816cbb80a48f9d33d4b
Carla Valdivia <valdivia.carla@gmail.com>
2024-12-19 16:55:20 -0300

    new/create for municipalities

--- app/controllers/municipalities_controller.rb
+++ app/controllers/municipalities_controller.rb
@@ -13,11 +13,30 @@ class MunicipalitiesController < ApplicationController
     end
   end
 
+  def new
+    @municipality = Municipality.new
+  end
+
+  def create
+    @municipality = Municipality.new(municipality_params)
+    if @municipality.save 
+      redirect_to(@municipality)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show 
     @municipality = Municipality.find(params[:id])
   end
 
   def meus_municipios 
     @municipalities = current_user.municipalities
+  end
+
+  private 
+
+  def municipality_params
+    params.require(:municipality).permit(:name, :contact_name, :contact_title, :original_coordinator, :number_of_attempts, :date_last_attempt, :contact_effective, :official_letter_sent, :capital_city, :state_id, :batch_id, :user_id)
   end
 end
