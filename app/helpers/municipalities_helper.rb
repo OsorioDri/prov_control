ee826caacc44741d4e5e657cf6be2929c9890d2c
Carla Valdivia <valdivia.carla@gmail.com>
2024-12-19 16:07:18 -0300

    show action for municipalities

--- app/helpers/municipalities_helper.rb
+++ app/helpers/municipalities_helper.rb
@@ -1,2 +1,9 @@
 module MunicipalitiesHelper
+  def icons(state)
+    if state
+      content_tag(:i, nil, class: 'fa-solid fa-square-check text-success')
+    else
+      content_tag(:i, nil, class: 'fa-solid fa-square-xmark text-danger')
+    end
+  end
 end
